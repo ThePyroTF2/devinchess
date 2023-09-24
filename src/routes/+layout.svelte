897e8e84c8259db88e6072a85ed46524edfa305c
Devin Droddy <devin.droddy@gmail.com>
2023-09-24 16:54:31 -0400

    add meta component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,10 @@
+<script>
+	import Meta from '$lib/components/meta.svelte'
+</script>
+
+<Meta
+	title="Devinchess"
+	description="Devinchess again. Sveltekit frontent + Rust backend. Currently WIP"
+/>
+
 <slot />
