46b525f552eaa6cef039c340c1c498d7cc3d2db0
Devin Droddy <devin.droddy@gmail.com>
2023-09-24 17:51:46 -0400

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import Meta from '$lib/components/meta.svelte'
+	import { dev } from '$app/environment'
+	import { inject } from '@vercel/analytics'
+
+	inject({ mode: dev ? 'development' : 'production' })
 </script>
 
 <Meta
